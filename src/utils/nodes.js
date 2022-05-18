d0b0be88acf1c0dd6bbaef299e9cba17de567ee4
david <davidalejo32@hotmail.com>
2022-05-18 16:38:08 -0500

    importacion de los nodos del DOM

---
+++ src/utils/nodes.js
@@ -0,0 +1,63 @@
+// importacion de los nodos del html
+
+// sections
+
+export const header = document.querySelector('.header');
+export const formSearch = document.querySelector('.form-search');
+export const recommendations = document.querySelector('.recommendations');
+export const categories = document.querySelector('.categories');
+export const popular = document.querySelector('.popular');
+export const search = document.querySelector('.search');
+export const movie = document.querySelector(".movie");
+export const genreByName = document.querySelector(".genreByName");
+export const seeAllPopular = document.querySelector(".seeAllPopular");
+
+
+// containers
+
+export const categoriesContainer = document.querySelector('.categories__container');
+export const popularMovies = document.querySelector('.popular__movies');
+export const movieHeader = document.querySelector('.movie__header');
+export const movieInfo = document.querySelector('.movie__info');
+export const movieDateContaine = document.querySelector('.movie__date-container');
+export const movieGenreContainer = document.querySelector('.movie__genre-container');
+export const movieVoteContainer = document.querySelector('.movie__vote-container');
+export const movieBackground = document.querySelector('.movie__background');
+export const genreByNameMovies = document.querySelector('.genreByName__movies');
+export const seeAllPopularContainer = document.querySelector(".seeAllPopular__container");
+export const movieRelatedContainer = document.querySelector(".movie__related-container");
+
+// buttons
+
+export const navHome = document.querySelector('.nav__home');
+export const navSearch = document.querySelector('.nav__search');
+export const formSearchButton = document.querySelector(".form-search__button");
+export const movieArrow = document.querySelector(".movie__arrow");
+export const genreByNameArrow = document.querySelector(".genreByName__arrow");
+export const popularAll = document.querySelector(".popular__all");
+export const popularArrow = document.querySelector(".seeAllPopular__arrow");
+export const genreByNameSeeMore = document.querySelector(".genreByName__seeMore");
+export const seeAllPopularSee = document.querySelector(".seeAllPopular__seeMore");
+
+
+// input
+
+export const formSearchInput = document.querySelector(".form-search__input");
+
+
+// h3
+export const movieTitle = document.querySelector(".movie__title");
+export const movieDescriptionTitle = document.querySelector(".movie__description-title");
+
+
+// img
+
+export const movieImg = document.querySelector(".movie__img");
+
+
+// p
+
+export const movieDateTitle = document.querySelector(".movie__date-title");
+export const movieGenreTitle = document.querySelector(".movie__genre-title");
+export const movieVote = document.querySelector(".movie__vote");
+export const moviedescription = document.querySelector(".movie__description");
