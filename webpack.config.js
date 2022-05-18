5742d54f51a0461537cfd3fe9031dc909d5566aa
david <davidalejo32@hotmail.com>
2022-05-18 16:42:12 -0500

    archivo de configuracion de webpack (build)

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const cssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const terserPlugin = require("terser-webpack-plugin");
+const dotEnv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const copyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+   mode: "production",
+   entry: "./src/index.js",
+   output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[contenthash].js",
+   },
+
+   resolve: {
+      extensions: [".js"],
+   },
+
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            use: {
+               loader: "babel-loader"
+            },
+            exclude: /node_modules/,
+
+         },
+
+         {
+            test: /\.css|.scss$/i,
+            use: [miniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+         },
+
+         {
+            test: /\.png$/i,
+            type: "asset/resource",
+            generator: {
+               filename: "assets/img/[hash][ext][query]"
+            }
+         },
+
+      ]
+   },
+
+   plugins: [
+      new htmlWebpackPlugin({
+         inject: true,
+         template: "./public/index.html",
+         filename: "./index.html",
+      }),
+
+      new miniCssExtractPlugin({
+         filename: '[name].[contenthash].css'
+      }),
+
+      new dotEnv(),
+
+      new CleanWebpackPlugin(),
+
+      new copyPlugin({
+         patterns: [
+            {
+               from: path.resolve(__dirname, 'src', 'img/'),
+               to: "src/img"
+            }
+         ]
+      })
+   ],
+
+   optimization: {
+      minimize: true,
+      minimizer: [
+         new cssMinimizerPlugin(),
+         new terserPlugin(),
+      ]
+   }
+
+}
